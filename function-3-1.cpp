583be0c4eac5f86e4e471d1b3281adc8c8e56d4b
ColinChan520 <104440913+ColinChan520@users.noreply.github.com>
2022-08-26 17:46:41 +0930

    1

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int negative_count(int *vals,int len){
     int count = 0;
     for (int i = 0; i < len; i++){
-        if (vals[i] < 0){
+        if (vals[i] <= 0){
             count++;
         }
     }
@@ -12,13 +12,13 @@ int negative_count(int *vals,int len){
 }
 
 int* extract_negatives(int *vals,int len, int num_negative){
-    int new_arr[num_negative];
+    int *negative = new int[num_negative];
     int j = 0;
     for (int i = 0; i < len; i++){
         if (vals[i] < 0){
-            new_arr[j] = vals[i];
+            negative[j] = vals[i];
             j++;
         }
     }
-    return new_arr;
+    return negative;
 }
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -6,20 +6,8 @@ extern int negative_count(int *vals,int len);
 extern int* extract_negatives(int *vals,int len, int num_negative);
 
 int main(){
-    int arr[] = {-5, -3, -1, 0, 1, 2, 3, -6};
-    int len = 8;
-    
-    cout << "number of negative integers in the given array are(calling negative_count first time):" << negative_count(arr, len) << endl;
-    cout << "number of negative integers in the given array are(calling negative_count second time):" << negative_count(arr, len) << endl;
-    
-    int num_negative = negative_count(arr,len);
-    int *a = extract_negatives(arr, len, num_negative);
-    
-    cout << "the array consisting of only negative values is: " << endl;
-    
-    for (int i = 0; i < num_negative; i++){
-        cout << *(a+i) << "";
-    }
-    cout << "\n\n";
+    int vals[] = {-2, -7, 5, 6, -8};
+    int num_negative = negative_count(vals, 5);
+    extract_negatives(vals, 5, num_negative);
     return 0;
 }
